985957116806a9ac827ad9714da1f7029b09ec5a
DominicNikolai <d960960gm@gmail.com>
2025-08-17 22:59:55 +0200

    v0.0.3

--- snap.js
+++ snap.js
@@ -1,6 +1,5 @@
 // Snap.svg 0.3.0
 // Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
-// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
